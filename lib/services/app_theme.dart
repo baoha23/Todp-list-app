e5f579c9e3ad1b26780ff3a72557afbeb84b4b96
Bảo Hà <118371199+baoha23@users.noreply.github.com>
2025-02-18 20:43:49 +0700

    Add files via upload

---
+++ lib/services/app_theme.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+enum AppTheme {
+  lightTheme,
+  darkTheme,
+}
+
+class AppThemes {
+  static final appThemeData = {
+    AppTheme.darkTheme: ThemeData(
+      primarySwatch: Colors.grey,
+      primaryColor: Colors.black,
+      brightness: Brightness.dark,
+      // ignore: deprecated_member_use
+      backgroundColor: const Color(0xFF212121),
+      dividerColor: Colors.black54,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+      ),
+      textTheme: const TextTheme(
+        // ignore: deprecated_member_use
+        subtitle1: TextStyle(color: Colors.white),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.grey, unselectedItemColor: Colors.white),
+      useMaterial3: true,
+    ),
+
+    //
+    //
+
+    AppTheme.lightTheme: ThemeData(
+      primarySwatch: Colors.grey,
+      primaryColor: Colors.white,
+      brightness: Brightness.light,
+      // ignore: deprecated_member_use
+      backgroundColor: const Color(0xFFE5E5E5),
+      dividerColor: const Color(0xff757575),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.black),
+        ),
+      ),
+      textTheme: const TextTheme(
+        // ignore: deprecated_member_use
+        subtitle1: TextStyle(color: Colors.black),
+      ),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.grey,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white),
+      useMaterial3: true,
+    ),
+  };
+}
---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:taskapp/models/task.dart';
+import 'task_tile.dart';
+
+class TasksList extends StatelessWidget {
+  // ignore: use_super_parameters
+  const TasksList({
+    Key? key,
+    required this.tasksList,
+  }) : super(key: key);
+
+  final List<Task> tasksList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
+          child: ListView.builder(
+              itemCount: tasksList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TaskTile(task: tasksList[index]),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
